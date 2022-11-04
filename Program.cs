0f1ed192d6c107b4e95663a361057377cfefcbde
Your Name <you@example.com>
2022-11-04 15:35:08 +0400

    ДЗ №2 (точка пересечения прямых)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿/* №1 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
 void FillArr (int[] array)
 {   int length = array.Length;
@@ -19,3 +19,21 @@ for (int j=0; j<massiv.Length; j++)
 }
 Console.WriteLine();
 Console.WriteLine(kol);
+*/
+
+// №2 точка пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2
+
+double x=0;
+double y=0;
+
+Console.WriteLine("Введите коэффициенты k1, b1, k2, b2 для двух прямых, заданным уравнением y=k*x+b");
+double k1 = double.Parse(Console.ReadLine());
+double b1 = double.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+
+if (k1==k2) Console.WriteLine("прямые не пересекаются");
+else
+{   x=(b2-b1)/(k1-k2);
+    y=k1*x + b1;
+    Console.WriteLine($"точка пересечения прямых ({x}; {y})");}
